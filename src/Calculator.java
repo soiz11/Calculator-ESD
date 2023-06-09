9b7c84e6476ccdb993573a9daa7d5450d9058a34
MadhuniYashodha <madhuniyashodha99@gmail.com>
2023-06-09 13:22:50 +0530

    add buttons

--- src/Calculator.java
+++ src/Calculator.java
@@ -224,4 +224,109 @@ public void addActionEvent() {
 
 @Override
 
+public void actionPerformed(ActionEvent e) {
+        Object source = e.getSource();
+        if (source == onRadioButton) {
+        enable();
+        } else if (source == offRadioButton) {
+        disable();
+        } else if (source == buttonClear) {
+        label.setText("");
+        textField.setText("");
+        } else if (source == buttonDelete) {
+        int length = textField.getText().length();
+        int number = length - 1;
+
+
+        if (length > 0) {
+        StringBuilder back = new StringBuilder(textField.getText());
+        back.deleteCharAt(number);
+        textField.setText(back.toString());
+
+        }
+        if (textField.getText().endsWith("")) {
+        label.setText("");
+        }
+        } else if (source == buttonZero) {
+        if (textField.getText().equals("0")) {
+        return;
+        } else {
+        textField.setText(textField.getText() + "0");
+        }
+        } else if (source == buttonOne) {
+        textField.setText(textField.getText() + "1");
+        } else if (source == buttonTwo) {
+        textField.setText(textField.getText() + "2");
+        } else if (source == buttonThree) {
+        textField.setText(textField.getText() + "3");
+        } else if (source == buttonFour) {
+        textField.setText(textField.getText() + "4");
+        } else if (source == buttonFive) {
+        textField.setText(textField.getText() + "5");
+        } else if (source == buttonSix) {
+        textField.setText(textField.getText() + "6");
+        } else if (source == buttonSeven) {
+        textField.setText(textField.getText() + "7");
+        } else if (source == buttonEight) {
+        textField.setText(textField.getText() + "8");
+        } else if (source == buttonNine) {
+        textField.setText(textField.getText() + "9");
+        } else if (source == buttonDot) {
+        if (textField.getText().contains(".")) {
+        return;
+        } else {
+        textField.setText(textField.getText() + ".");
+        }
+
+        } else if (source == buttonPlus) {
+        String str = textField.getText();
+        number = Double.parseDouble(textField.getText());
+        textField.setText("");
+        label.setText(str + "+");
+        calculation = 1;
+        } else if (source == buttonMinus) {
+        String str = textField.getText();
+        number = Double.parseDouble(textField.getText());
+        textField.setText("");
+        label.setText(str + "-");
+        calculation = 2;
+        } else if (source == buttonMul) {
+        String str = textField.getText();
+        number = Double.parseDouble(textField.getText());
+        textField.setText("");
+        label.setText(str + "X");
+        calculation = 3;
+        } else if (source == buttonDiv) {
+        String str = textField.getText();
+        number = Double.parseDouble(textField.getText());
+        textField.setText("");
+        label.setText(str + "/");
+        calculation = 4;
+        } else if (source == buttonSqrt) {
+        number = Double.parseDouble(textField.getText());
+        Double sqrt = Math.sqrt(number);
+        textField.setText(Double.toString(sqrt));
+
+        } else if (source == buttonSquare) {
+        String str = textField.getText();
+        number = Double.parseDouble(textField.getText());
+        double square = Math.pow(number, 2);
+        String string = Double.toString(square);
+        if (string.endsWith(".0")) {
+        textField.setText(string.replace(".0", ""));
+        } else {
+        textField.setText(string);
+        }
+        label.setText("(sqr)" + str);
+        } else if (source == buttonReciprocal) {
+        number = Double.parseDouble(textField.getText());
+        double reciprocal = 1 / number;
+        String string = Double.toString(reciprocal);
+        if (string.endsWith(".0")) {
+        textField.setText(string.replace(".0", ""));
+        } else {
+        textField.setText(string);
+        }
+        } else if (source == buttonEqual) {
+        switch (calculation) {
 
